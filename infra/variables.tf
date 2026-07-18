2543dd7f68ef9245f15db6a3d8b46035171716e4
Selvakumar Perumal <selvakumarp@proton.me>
2026-07-18 21:42:33 +0530

    fix(infra): scale EKS cluster nodes to 2 to increase pod capacity

--- infra/variables.tf
+++ infra/variables.tf
@@ -44,8 +44,8 @@ variable "eks_managed_node_groups" {
     default = {
       instance_types = ["t3.medium"]
       min_size       = 1
-      max_size       = 2
-      desired_size   = 1
+      max_size       = 3
+      desired_size   = 2
     }
   }
 }
